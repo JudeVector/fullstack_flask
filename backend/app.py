9eed9b12c73e451d7a12a24c8ae079575832d932
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-01-11 15:48:25 +0400

    Updated the app.py to use uuid as id

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
+import uuid
 
 app = Flask(__name__)
 CORS(app)
@@ -11,9 +12,14 @@ db = SQLAlchemy(app)
 
 class Topic(db.Model):
     __tablename__ = "topics"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True)
     title = db.Column(db.String(120))
     description = db.Column(db.String(120))
+
+    def __init__(self, title, description):
+        self.id = str(uuid.uuid4())  # Generate UUID on object creation
+        self.title = title
+        self.description = description
 
     def json(self):
         return {"id": self.id, "title": self.title, "description": self.description}
@@ -22,13 +28,11 @@ class Topic(db.Model):
 db.create_all()
 
 
-# Create a test route
 @app.route("/test", methods=["GET"])
 def test():
     return jsonify({"message": "The server is running"})
 
 
-# Create a user route
 @app.route("/api/v1/topics", methods=["POST"])
 def create_topic():
     try:
@@ -53,23 +57,21 @@ def create_topic():
         )
 
 
-# Get all users
 @app.route("/api/v1/topics", methods=["GET"])
 def get_topic():
     try:
         topics = Topic.query.all()
         topics_data = [
-            {"id": topics.id, "title": topics.title, "description": topics.description}
-            for topics in topics
+            {"id": topic.id, "title": topic.title, "description": topic.description}
+            for topic in topics
         ]
         return jsonify(topics_data), 200
     except Exception as e:
         return make_response(
-            jsonify({"message": "Error getting users", "error": str(e)}), 500
+            jsonify({"message": "Error getting topics", "error": str(e)}), 500
         )
 
 
-# Get a user by id
 @app.route("/api/v1/topics/<id>", methods=["GET"])
 def get_topic_id(id):
     try:
@@ -83,7 +85,6 @@ def get_topic_id(id):
         )
 
 
-# Update user by id
 @app.route("/api/v1/topics/<id>", methods=["PUT"])
 def update_topic(id):
     try:
@@ -93,15 +94,14 @@ def update_topic(id):
             topic.title = data["title"]
             topic.description = data["description"]
             db.session.commit()
-            return make_response(jsonify({"message": "topic updated"}), 200)
-        return make_response(jsonify({"message": "topic not found"}), 404)
+            return make_response(jsonify({"message": "Topic updated"}), 200)
+        return make_response(jsonify({"message": "Topic not found"}), 404)
     except Exception as e:
         return make_response(
             jsonify({"message": "Internal Server Error", "error": str(e)}), 500
         )
 
 
-# Delete a user
 @app.route("/api/v1/topics/<id>", methods=["DELETE"])
 def delete_topic(id):
     try:
